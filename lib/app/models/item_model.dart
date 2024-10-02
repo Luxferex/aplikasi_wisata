3cd8feb0af226ca44dd3c610e0b27d44ab8368e5
Fausta Akbar Wijaya <faussss1357@gmail.com>
2024-10-02 20:06:49 +0700

    Create models

---
+++ lib/app/models/item_model.dart
@@ -0,0 +1,12 @@
+class Item {
+  String image;
+  String title;
+  String description;
+  bool like;
+
+  Item(
+      {required this.image,
+      required this.title,
+      required this.description,
+      required this.like});
+}
