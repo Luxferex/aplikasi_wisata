5c2681fb388b0427a2b6eaa2030b46d65845e471
Fausta Akbar Wijaya <faussss1357@gmail.com>
2024-10-04 08:21:04 +0700

    Add maps feature 1.1

--- lib/app/modules/maps/bindings/maps_bindings.dart
+++ lib/app/modules/maps/bindings/maps_bindings.dart
@@ -1,4 +1,4 @@
-import 'package:aplikasi_wisata/app/modules/detail/views/maps_view.dart';
+import 'package:aplikasi_wisata/app/modules/maps/views/maps_view.dart';
 import 'package:get/get.dart';
 
 class MapsBindings extends Bindings {
